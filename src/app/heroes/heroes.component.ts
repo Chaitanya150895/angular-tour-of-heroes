d19922ed9cfc4443c894a84f30c97a8f2eb55de5
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-02 17:09:29 +0530

    two way of declaring variable // commented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,13 +6,20 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  
-  constructor() { }
+
+  constructor() { 
+ 
+   // this.hero = new Hero();
+   // this.hero.id = 1;
+   // this.hero.name = 'windstrom';
+   //this above is a way to declare the value of a variable as string was converted to the object
+  }
 
   ngOnInit() {
   }
