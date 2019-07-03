60d38614d792aa470fbf58d67ddb11bd30b61213
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-03 19:30:37 +0530

    created a dummy server for getting and returning the data // abandoned the local data instead

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-
+ 
 import { Observable, of } from 'rxjs';
-
+ 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
-
-@Injectable({
-  providedIn: 'root',
-})
+ 
+@Injectable({ providedIn: 'root' })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
-
+  private heroesUrl = 'api/heroes';
+  constructor(private http:HttpClient, private messageService: MessageService) { }
+ 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
+ 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url);
+    }
 }
