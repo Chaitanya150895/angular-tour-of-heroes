984b5dd487a97ad4f1b4a46bb5b7678ee81f0c80
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-24 12:03:57 +0530

    Testing pipes and toggle date format and chaining the pipe

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
- 
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
- 
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero; 
+  @Input() hero: Hero;
 
+  birthday = new Date(1988, 3, 15); // April 15, 1988
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
+  
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
   ) {}
- 
+
   ngOnInit(): void {
     this.getHero();
   }
- 
+
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
- 
+
   goBack(): void {
     this.location.back();
   }
